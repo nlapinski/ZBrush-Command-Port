fac672e0bf52ad7b59dacca138e5fbc3ccc5bfdc
Nate Lapinski <natelapinski@gmail.com>
2013-07-07 23:05:35 -0400

    updated zbrush server

--- zbrush_scripts/zserv.py
+++ zbrush_scripts/zserv.py
@@ -29,14 +29,38 @@ def zbrush_open(name):
     env = os.getenv('ZDOCS', "/your/file/path/for/zbrush/to/open/files")
     print env
 
-    zscript = '[RoutineDef, open_file,'
-    zscript += '[FileNameSetNext,"!:'
-    zscript += os.path.join(env, name)
-    zscript += '"]'
-    zscript += '[IPress,Tool:Import]'
-    zscript += ']'
-    zscript += '[RoutineCall,open_file]'
-    print zscript
+    zscript = """
+            [RoutineDef, open_file,
+            [FileNameSetNext,"!:#FILENAME"]
+            [VarSet,in_tool,#TOOLNAME]
+            [VarSet,imp,0]
+            [Loop, [SubToolGetCount],
+            [VarSet, a, a+1]
+            [SubToolSelect,a-1]
+            [VarSet, sub, [FileNameExtract,[GetActiveToolPath],2]]
+            [If, [StrFind, in_tool, sub]>-1,
+                [IPress,Tool:Import]
+                [VarSet,imp,1],]
+            ]
+            [If, imp<1,
+                    [If, a==[SubToolGetCount],
+
+
+                        [IPress,Tool:SubTool:Duplicate]
+                        [IPress,Tool:SubTool:MoveDown]
+                        [IPress,Tool:Geometry:Del Higher]
+                        [IPress,Tool:Import]
+                        [ToolSetPath,[SubToolGetCount],"!:#FILENAME"]
+                        , [MessageOk, False]
+                    ]
+            ]
+            ]
+            [RoutineCall,open_file]
+                """
+
+    zscript = zscript.replace('#FILENAME',os.path.join(env, name))
+    zscript = zscript.replace('#TOOLNAME',name.replace('.ma',''))
+    #print zscript
     zs_temp.write(zscript)
     return zs_temp.name
 
@@ -58,10 +82,11 @@ def listen():
         if not data:
             break
         if(data.split('|')[0] == 'open'):
-            zs_temp = zbrush_open(data.split('|')[1])
-            print zs_temp
-            print data.split('|')[1]
-            send_osa(zs_temp)
+            objs = data.split('|')[1].split(':')
+            for obj in objs:
+                print obj
+                zs_temp = zbrush_open(obj+'.ma')
+                send_osa(zs_temp)
 
     conn.close()
     print "end"
