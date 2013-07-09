e637a9ad9491ae70aa11cce5d69705ef2ccfd84a
Nate Lapinski <natelapinski@gmail.com>
2013-07-09 16:57:15 -0400

    added consts for env vars, moved maya commands to zclient.main added a timeout to lock file check

--- mclient/zbrush_export.py
+++ mclient/zbrush_export.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
-from multiprocessing import Process
 import sys
 import socket
 import os
 from subprocess import *
 from tempfile import *
 import time
+
+SHARED_DIR_ENV='$ZDOCS'
 
 
 def send_osa(script_path):
@@ -19,11 +20,9 @@ def send_osa(script_path):
     cmd = ' '.join(cmd)
     print cmd
     script = Popen(cmd, shell=True)
-    # script.communicate()
-    # Popen(cmd.replace('.txt','.zsc'))
 
 
-def zbrush_save(file, env, tool):
+def zbrush_save(file, tool):
     """save a file from zbrush
 
     -creates a tmp file to load with zscript
@@ -34,16 +33,13 @@ def zbrush_save(file, env, tool):
 
     """
 
-    # FIXME: don't pass the environment variable name, define it in a constant
     zs_temp = NamedTemporaryFile(delete=False, suffix='.txt')
-    env_expand = os.path.expandvars(env)
-    print env
+    env_expand = os.path.expandvars(SHARED_DIR_ENV)
 
     zscript = '[RoutineDef, save_f,'
     zscript += '[SubToolSelect,'+tool+']'
     zscript += '[FileNameSetNext,"!:'
     zscript += os.path.join(env_expand, file + '.ma')
-    print file
     zscript += '","ZSTARTUP_ExportTamplates\Maya.ma"]'
     zscript += '[IPress,Tool:Export]'
     zscript += '[MemCreate, zzz, 1, 0]'
@@ -55,70 +51,44 @@ def zbrush_save(file, env, tool):
     zscript += '[Exit]'
     zscript += ']'
     zscript += '[RoutineCall,save_f]'
-    print zscript
     zs_temp.write(zscript)
     return zs_temp.name
 
 
-def send_to_maya(file, env):
+def send_to_maya(file):
     """send a file to maya
 
-    -cleans up past objects/shaders/textures
-    -removes object from maya if it exists
-    -loads object saved from zbrush
+    -calls get_from_zbrush() in zclient.main in maya
     -removes zbrush temp lock file
-
+    -grabs the mnet env for IP/PORT or defaults 
     """
-    # FIXME: don't pass the environment variable name, define it in a constant
-    env = '$' + env
 
-    file_path = os.path.join(env, file + '.ma')
+    file_path = os.path.join(SHARED_DIR_ENV, file + '.ma')
 
-    # FIXME: put all this code in a function that maya can import and execute on the other side
-    mayaCMD = 'import maya.cmds as cmds'
+    print file_path
+    mayaCMD = 'import zclient'
     mayaCMD += '\n'
-    mayaCMD += 'import maya.mel as mel'
-    mayaCMD += '\n'
-    mayaCMD += 'try:'
-    mayaCMD += '\n'
-    mayaCMD += '    cmds.delete("' + file + '")'
-    mayaCMD += '\n'
-    mayaCMD += 'except:'
-    mayaCMD += '\n'
-    mayaCMD += '    print "error"'
-    mayaCMD += '\n'
-    mayaCMD += 'try:'
-    mayaCMD += '\n'
-    mayaCMD += '    cmds.delete("'+file+'_blinn")'
-    mayaCMD += '\n'
-    mayaCMD += '    cmds.delete("'+file+'_blinnSG")'
-    mayaCMD += '\n'
-    mayaCMD += '    cmds.delete("'+file+'_materialInfo")'
-    mayaCMD += '\n'
-    mayaCMD += '    cmds.delete("'+file+'_ZBrushTexture")'
-    mayaCMD += '\n'
-    mayaCMD += '    cmds.delete("'+file+'_place2dTexture2")'
-    mayaCMD += '\n'
-    mayaCMD += 'except:'
-    mayaCMD += '\n'
-    mayaCMD += '    print "'+file+'_blinn'+ ' does not exist"'
-    mayaCMD += '\n'
-    mayaCMD += 'print "' + file_path + '"'
-    mayaCMD += '\n'
-    mayaCMD += 'cmds.file("' + file_path + '",i=True,uns=False,rdn=True)'
-    mayaCMD += '\n'
-    mayaCMD += 'print "SENT"'
-    mayaCMD += '\n'
+    mayaCMD += 'zclient.main.get_from_zbrush("'+file_path+'")'
 
     # wait until the zbrush file has finished saving before sending
     # the import command to maya
+    
     lock_file = os.path.expandvars(file_path).replace('.ma', '.zzz')
-    # FIXME: add a timeout here to avoid infinite loop
+    #added timeout check to prevent loop from breaking
+    time_out = time.time()
     while os.path.isfile(lock_file) == False:
         print 'waiting'
-    os.remove(lock_file)
+        time.sleep(1)
+        if time.time()>time_out:
+            print 'timeout'
+            break
+    try:
+        os.remove(lock_file)
+    except:
+        print 'no lock file, sending anyway'
     maya = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   
+    print mayaCMD
+
     mnet = os.getenv('MNET', "192.168.1.20:6667")
     host = mnet.split(':')[0]
     port = mnet.split(':')[1]
@@ -140,10 +110,9 @@ if __name__ == "__main__":
     file = (sys.argv)[1]
     tool = (sys.argv)[2]
     func = (sys.argv)[3]
-    env = '$ZDOCS'
 
     if func == '0':
-        zs_temp = zbrush_save(file, env,tool)
+        zs_temp = zbrush_save(file,tool)
         send_osa(zs_temp)
     if func == '1':
-        send_to_maya(file,env.replace('$',''))
+        send_to_maya(file)
