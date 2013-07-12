fcb29c4fc3e28ac15fc2c321c78b250d43a4e4bc
Nate Lapinski <natelapinski@gmail.com>
2013-07-12 19:00:26 -0400

    removed waiting and lock file

--- mclient/zbrush_export.py
+++ mclient/zbrush_export.py
@@ -19,7 +19,6 @@ def send_to_maya(file):
     """send a file to maya
 
     -calls get_from_zbrush() in zclient.main in maya
-    -removes zbrush temp lock file
     -grabs the mnet env for IP/PORT or defaults 
     """
 
@@ -29,27 +28,8 @@ def send_to_maya(file):
     mayaCMD = 'import zclient'
     mayaCMD += '\n'
     mayaCMD += 'zclient.main.get_from_zbrush("'+file_path+'")'
-
-    # wait until the zbrush file has finished saving before sending
-    # the import command to maya
     
-    lock_file = os.path.expandvars(file_path).replace('.ma', '.zzz')
-    #added timeout check to prevent loop from breaking
-    time_out = time.time()+5
-    
-    while os.path.isfile(lock_file) == False:
-        print 'waiting'
-        time.sleep(1)
-        if time.time()>time_out:
-            print 'timeout'
-            break
-    try:
-        os.remove(lock_file)
-    except:
-        print 'no lock file, sending anyway'
-
     maya = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print mayaCMD
 
     # FIXME: get rid of this default value. error if MNET is not set
     mnet = os.getenv('MNET', "192.168.1.20:6667")
